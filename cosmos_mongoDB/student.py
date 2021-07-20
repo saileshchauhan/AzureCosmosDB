6bc43bc346be77a43527b4b36e6f6f12641cd177
saileshchauhan <chauhansailesh68@gmail.com>
2021-07-20 16:21:10 +0530

    [sailesh] add: mongodb_API

---
+++ cosmos_mongoDB/student.py
@@ -0,0 +1,11 @@
+sam_item={
+        "_id": "1",
+        "class": "first",
+        "name": "sam"
+    }
+
+sandra_item={
+        "_id": "2",
+        "class": "third",
+        "name": "sandra"
+    }
